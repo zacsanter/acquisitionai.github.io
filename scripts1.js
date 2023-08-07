54642b67ae06159d0fea90f3cfe903d19a8ead2a
zacsanter <53116184+zacsanter@users.noreply.github.com>
2023-08-07 20:48:37 +0100

    Update scripts1.js

--- scripts1.js
+++ scripts1.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const voiceflowVersionID =
     document.getElementById('vfassistant').getAttribute('data-version') ||
     'production'
-  const voiceflowAPIKey = 'VF.DM.64cd71cde8300a00077e246c.5PFuJPx3R8kLZoKe'
+  const voiceflowAPIKey = 'VF.DM.64d0df22cc248300068a858c.KXeO554glAybHVsR'
 
   let audio = new Audio()
   const input = document.getElementById('user-input')
