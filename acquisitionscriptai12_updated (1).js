826fe430e4ce84af7d3e8b5cfb8f59b8841ccdeb
zacsanter <53116184+zacsanter@users.noreply.github.com>
2023-08-11 21:26:11 +0100

    Update acquisitionscriptai12_updated (1).js

--- acquisitionscriptai12_updated (1).js
+++ acquisitionscriptai12_updated (1).js
@@ -73,7 +73,8 @@ function displayResponse(response) {
 }
 
 async function sendToWebhook(data) {
-    const webhookUrl = "YOUR_WEBHOOK_ENDPOINT";  // Replace with your webhook URL
+    const webhookUrl = "https://hook.eu2.make.com/s99gs8nxfqaz2r49iiwkttzjgk3sl4k6
+";  // Replace with your webhook URL
 
     try {
         const response = await fetch(webhookUrl, {
